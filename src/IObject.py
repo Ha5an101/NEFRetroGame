866cc0490847c67a3fe305135793f05c2b7737fa
AlfredIsAPicoByte <19atang-agama.a@nef.tynecoast.academy>
2025-03-01 14:03:52 +0000

    Concatenate surface and objec interfaces

--- src/IObject.py
+++ src/IObject.py
@@ -1,9 +1,40 @@
+import pygame as pg
 import numpy as np
 
+class SurfaceObject:
+	def __init__(self, width: int, height: int):
+		self.surface = pg.Surface((width, height))
+		self.rect = self.surface.get_rect(topleft=(0, 0))
+
+    def DrawSurface(self, screen):
+		screen.blit(self.surface, self.rect)
+
+    def SetSurface(self, surface: pg.Surface):
+		self.surface = surface
+
+    def TransformRect(self, **kwargs):
+		self.rect = self.surface.get_rect(kwargs)
+
+class ImageObject(SurfaceObject):
+	def __init__ (self, path: str):
+		self.surface = pg.image.load(path).convert_alpha()
+		self.rect = self.copy_surface.get_rect(topleft=(0, 0))
+		
+	def SetSurface(self, surface: pg.image):
+		self.surface = surface
+
+class TextObject(SurfaceObject):
+	def __init__ (self, path: str, size: int):
+		self.surface = pg.font.Font(path, size)
+		self.rect = self.copy_surface.get_rect(topleft=(0, 0))
+		
+	def SetSurface(self, surface: pg.font):
+		self.surface = surface
+
 class GameObject:
-    def __init__(self, pos: list[int], dirc: list[int]):
-        self.position = pos
-        self.direction = dirc
+    def __init__(self, position: list[int], direction: list[int]):
+        self.position = position
+        self.direction = direction
     
     def SetPosition(self, new_pos: list[int]):
         self.position = new_pos
